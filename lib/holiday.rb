257944a71f511c3c38f661cfec0bd250d9ad8f80
Sharon Watkins <swatkins876@gmail.com>
2019-09-16 20:06:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do | season, holiday|
+     puts "#{season.capitalize}:"
 end
 
 def all_holidays_with_bbq(holiday_hash)
